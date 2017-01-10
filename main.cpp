9886e190af124321d74cd7db5e56c6fc44f42c9e
Enrique Lazcorreta Puigmartí <enrique@umh.es>
2017-01-10 13:57:41 +0100

    Pasos iniciales, primeros esbozos.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,55 @@
 //
 
 #include <iostream>
+#include <fstream>
+using std::ifstream;
+#include <string>
+using std::string;
+using std::cout;
+using std::endl;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool m_parlanchin = true;
+
+class acArchivo : public ifstream
+{
+    string m_nombre;
+public:
+    acArchivo(string nombre) : m_nombre(nombre)
+    {
+        open(m_nombre);
+        if (!is_open())
+            throw ("No he podido abrir el archivo " + m_nombre);
+        if (m_parlanchin) {
+            cout << "Archivo " << m_nombre << " abierto" << endl;
+        }
+    }
+    ~acArchivo() { close(); }
+};
+
+
+
+
+
+
+
+
+
+
+int main(int argc, const char * argv[])
+{
+    acArchivo *archivo;
+    try
+    {
+        archivo = new acArchivo("/Users/enriquelazcorretapuigmarti/Dropbox/investigacion-2016/ACDC/datos/_KEEL/abalone.dat");
+    }
+    catch (string &e)
+    {
+        cout << e << endl;
+        return 1;
+    }
+
+    delete archivo;
+    
+    cout << "Hello, World!\n";
     return 0;
 }
