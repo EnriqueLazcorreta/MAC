92d5f91aaa08fd15aabdd97d9600bf7963a37eea
Enrique Lazcorreta Puigmartí <enrique@umh.es>
2017-01-10 13:08:43 +0100

    Pasos iniciales, comenzando desde cero.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MAC
+//
+//  Created by Enrique Lazcorreta Puigmartí on 10/1/17.
+//  Copyright © 2017 Enrique Lazcorreta Puigmartí. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
